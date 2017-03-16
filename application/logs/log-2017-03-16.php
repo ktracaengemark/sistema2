6ea041b63aaaf684e9a22dc17febdbf093fb3d90
ktracaengemark-Itau <marciorodeng@gmail.com>
2017-03-16 16:32:44 -0300

    Ajustes e correção do erro do cadastro profissional

---
+++ application/logs/log-2017-03-16.php
@@ -0,0 +1,8 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2017-03-16 19:43:56 --> 404 Page Not Found: Relatorio/arquivos
+ERROR - 2017-03-16 19:44:02 --> Query error: Unknown column 'arquivos' in 'where clause' - Invalid query: SELECT * FROM App_Empresa WHERE idApp_Empresa = arquivos
+ERROR - 2017-03-16 19:44:02 --> 404 Page Not Found: Faviconico/index
+ERROR - 2017-03-16 19:44:21 --> 404 Page Not Found: Cliente/arquivos
+ERROR - 2017-03-16 19:44:32 --> 404 Page Not Found: Relatorio/arquivos
+ERROR - 2017-03-16 19:44:38 --> Query error: Unknown column 'arquivos' in 'where clause' - Invalid query: SELECT * FROM App_Empresa WHERE idApp_Empresa = arquivos
