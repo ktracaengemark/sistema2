b98531481e08ff26cc37ac82dd128acdac33e6b9
romatiservicos <abreu.victorrr@gmail.com>
2017-03-31 13:28:17 -0300

    Implementação do botão agenda no mobile

---
+++ application/logs/log-2017-03-31.php
@@ -0,0 +1,21 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2017-03-31 18:23:14 --> Severity: Notice --> Undefined variable: nav_secundario C:\xampp\htdocs\sistema2\application\views\tarefa\tela_tarefa.php 6
+ERROR - 2017-03-31 18:23:14 --> 404 Page Not Found: Tarefa/arquivos
+ERROR - 2017-03-31 18:23:27 --> 404 Page Not Found: Tarefa/arquivos
+ERROR - 2017-03-31 18:24:41 --> Severity: Notice --> Undefined variable: nav_secundario C:\xampp\htdocs\sistema2\application\views\tarefa\tela_tarefa.php 6
+ERROR - 2017-03-31 18:24:41 --> 404 Page Not Found: Tarefa/arquivos
+ERROR - 2017-03-31 18:25:53 --> Severity: Notice --> Undefined variable: nav_secundario C:\xampp\htdocs\sistema2\application\views\tarefa\tela_tarefa.php 6
+ERROR - 2017-03-31 18:25:53 --> 404 Page Not Found: Tarefa/arquivos
+ERROR - 2017-03-31 18:26:14 --> Severity: Notice --> Undefined variable: nav_secundario C:\xampp\htdocs\sistema2\application\views\tarefa\tela_tarefa.php 6
+ERROR - 2017-03-31 18:26:14 --> 404 Page Not Found: Tarefa/arquivos
+ERROR - 2017-03-31 18:26:37 --> Severity: Notice --> Undefined variable: nav_secundario C:\xampp\htdocs\sistema2\application\views\tarefa\tela_tarefa.php 6
+ERROR - 2017-03-31 18:26:37 --> 404 Page Not Found: Tarefa/arquivos
+ERROR - 2017-03-31 18:26:50 --> Severity: Notice --> Undefined index: Cliente C:\xampp\htdocs\sistema2\application\views\tarefa\form_tarefa.php 332
+ERROR - 2017-03-31 18:26:50 --> 404 Page Not Found: Tarefa/arquivos
+ERROR - 2017-03-31 18:27:00 --> Severity: Notice --> Undefined index: Cliente C:\xampp\htdocs\sistema2\application\views\tarefa\form_tarefa.php 332
+ERROR - 2017-03-31 18:27:00 --> 404 Page Not Found: Tarefa/arquivos
+ERROR - 2017-03-31 18:27:35 --> Severity: Notice --> Undefined index: Cliente C:\xampp\htdocs\sistema2\application\views\tarefa\form_tarefa.php 335
+ERROR - 2017-03-31 18:27:35 --> 404 Page Not Found: Tarefa/arquivos
+ERROR - 2017-03-31 18:27:48 --> Severity: Notice --> Undefined variable: nav_secundario C:\xampp\htdocs\sistema2\application\views\tarefa\tela_tarefa.php 6
+ERROR - 2017-03-31 18:27:48 --> 404 Page Not Found: Tarefa/arquivos
