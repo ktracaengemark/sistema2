af2f6074871d582569b1dd98b8c19ce805548f4f
ktracaengemark-Itau <marciorodeng@gmail.com>
2017-04-15 07:47:02 -0300

    atualizações do Rodrigo nos relatórios
    Filtro por nome

--- application/logs/log-2017-04-15.php
+++ application/logs/log-2017-04-15.php
@@ -6,3 +6,6 @@ ERROR - 2017-04-15 11:14:28 --> Severity: Notice --> Undefined index: Cliente C:
 ERROR - 2017-04-15 11:14:53 --> Severity: Notice --> Undefined variable: nav_secundario C:\xampp\htdocs\sistema2\application\views\tarefa\tela_tarefa.php 6
 ERROR - 2017-04-15 11:17:19 --> 404 Page Not Found: Consulta/arquivos
 ERROR - 2017-04-15 11:18:20 --> 404 Page Not Found: Consulta/arquivos
+ERROR - 2017-04-15 11:31:05 --> Query error: Unknown column 'arquivos' in 'where clause' - Invalid query: SELECT * FROM App_Consulta WHERE idApp_Consulta = arquivos
+ERROR - 2017-04-15 11:31:06 --> 404 Page Not Found: Faviconico/index
+ERROR - 2017-04-15 11:31:16 --> Severity: Notice --> Undefined index: DataFim C:\xampp\htdocs\sistema2\application\controllers\Consulta.php 469
