f16f2ba5aece03482173e030ebc445e48b9fbd3f
ktracaengemark-Itau <marciorodeng@gmail.com>
2017-05-07 10:20:36 -0300

    Ajustes no layout

---
+++ application/logs/log-2017-05-05.php
@@ -0,0 +1,17 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2017-05-05 23:23:03 --> Query error: Unknown column 'arquivos' in 'where clause' - Invalid query: SELECT * FROM App_Consulta WHERE idApp_Consulta = arquivos
+ERROR - 2017-05-05 23:23:05 --> 404 Page Not Found: Faviconico/index
+ERROR - 2017-05-05 23:23:27 --> Query error: Unknown column 'arquivos' in 'where clause' - Invalid query: SELECT * FROM App_Cliente WHERE idApp_Cliente = arquivos
+ERROR - 2017-05-05 23:23:47 --> Query error: Unknown column 'arquivos' in 'where clause' - Invalid query: SELECT * FROM App_Consulta WHERE idApp_Consulta = arquivos
+ERROR - 2017-05-05 23:23:47 --> 404 Page Not Found: Consulta/favicon.ico
+ERROR - 2017-05-05 23:23:56 --> Severity: Notice --> Undefined index: query C:\xampp\htdocs\sistema2\application\controllers\Consulta.php 370
+ERROR - 2017-05-05 23:23:57 --> Severity: Notice --> Undefined index: redirect C:\xampp\htdocs\sistema2\application\controllers\Consulta.php 377
+ERROR - 2017-05-05 23:24:09 --> Query error: Unknown column 'arquivos' in 'where clause' - Invalid query: SELECT * FROM App_Consulta WHERE idApp_Consulta = arquivos
+ERROR - 2017-05-05 23:24:16 --> Severity: Notice --> Undefined index: query C:\xampp\htdocs\sistema2\application\controllers\Consulta.php 370
+ERROR - 2017-05-05 23:24:17 --> Severity: Notice --> Undefined index: redirect C:\xampp\htdocs\sistema2\application\controllers\Consulta.php 377
+ERROR - 2017-05-05 23:24:32 --> 404 Page Not Found: Consulta/arquivos
+ERROR - 2017-05-05 23:25:40 --> Query error: Unknown column 'arquivos' in 'where clause' - Invalid query: SELECT * FROM App_Consulta WHERE idApp_Consulta = arquivos
+ERROR - 2017-05-05 23:28:27 --> Query error: Unknown column 'arquivos' in 'where clause' - Invalid query: SELECT * FROM App_Consulta WHERE idApp_Consulta = arquivos
+ERROR - 2017-05-05 23:28:41 --> Query error: Unknown column 'arquivos' in 'where clause' - Invalid query: SELECT * FROM App_Cliente WHERE idApp_Cliente = arquivos
+ERROR - 2017-05-05 23:28:53 --> Query error: Unknown column 'arquivos' in 'where clause' - Invalid query: SELECT * FROM App_Consulta WHERE idApp_Consulta = arquivos
