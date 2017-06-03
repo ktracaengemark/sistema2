fb8e5271f944fa6769e7eff4f8399d6a32514901
ktracaengemark-Itau <marciorodeng@gmail.com>
2017-06-03 12:20:20 -0300

    Ajustes da Logo do sistema

--- application/logs/log-2017-06-03.php
+++ application/logs/log-2017-06-03.php
@@ -132,3 +132,53 @@ ERROR - 2017-06-03 14:36:24 --> 404 Page Not Found: Despesas/arquivos
 ERROR - 2017-06-03 14:37:41 --> 404 Page Not Found: Servico/arquivos
 ERROR - 2017-06-03 14:39:52 --> Severity: Notice --> Undefined index: QuitadoOrca C:\xampp\htdocs\sistema2\application\views\relatorio\tela_orcamentopc.php 93
 ERROR - 2017-06-03 14:39:52 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\sistema2\application\views\relatorio\tela_orcamentopc.php 93
+ERROR - 2017-06-03 15:57:15 --> 404 Page Not Found: Cliente/arquivos
+ERROR - 2017-06-03 15:59:37 --> 404 Page Not Found: Cliente/arquivos
+ERROR - 2017-06-03 16:00:23 --> 404 Page Not Found: Cliente/arquivos
+ERROR - 2017-06-03 16:02:40 --> Severity: Notice --> Undefined variable: data C:\xampp\htdocs\sistema2\application\controllers\Login.php 479
+ERROR - 2017-06-03 16:05:46 --> Severity: Notice --> Undefined index: Cliente C:\xampp\htdocs\sistema2\application\views\despesas\form_despesas.php 678
+ERROR - 2017-06-03 16:08:22 --> Severity: Notice --> Undefined index: Cliente C:\xampp\htdocs\sistema2\application\views\despesas\form_despesas.php 678
+ERROR - 2017-06-03 16:21:43 --> Severity: Notice --> Undefined index: Cliente C:\xampp\htdocs\sistema2\application\views\tarefa\form_tarefa.php 321
+ERROR - 2017-06-03 16:21:43 --> Severity: Notice --> Undefined index: idApp_Procedtarefa C:\xampp\htdocs\sistema2\application\views\tarefa\form_tarefa.php 324
+ERROR - 2017-06-03 16:21:43 --> Severity: Notice --> Undefined variable: parcelasrec C:\xampp\htdocs\sistema2\application\views\tarefa\form_tarefa.php 325
+ERROR - 2017-06-03 16:21:45 --> Query error: Unknown column 'arquivos' in 'where clause' - Invalid query: SELECT * FROM App_Tarefa WHERE idApp_Tarefa = arquivos
+ERROR - 2017-06-03 16:23:57 --> Severity: Notice --> Undefined index: Cliente C:\xampp\htdocs\sistema2\application\views\tarefa\form_tarefa.php 321
+ERROR - 2017-06-03 16:23:57 --> Severity: Notice --> Undefined index: idApp_Procedtarefa C:\xampp\htdocs\sistema2\application\views\tarefa\form_tarefa.php 324
+ERROR - 2017-06-03 16:23:57 --> Severity: Notice --> Undefined variable: parcelasrec C:\xampp\htdocs\sistema2\application\views\tarefa\form_tarefa.php 325
+ERROR - 2017-06-03 16:26:55 --> 404 Page Not Found: Cliente/arquivos
+ERROR - 2017-06-03 16:30:12 --> Query error: Unknown column 'arquivos' in 'where clause' - Invalid query: SELECT OT.idApp_OrcaTrata, OT.DataOrca, OT.DataPrazo, OT.ProfissionalOrca, OT.AprovadoOrca, OT.ObsOrca FROM App_OrcaTrata AS OT WHERE OT.idApp_Cliente = arquivos AND OT.AprovadoOrca = "S" ORDER BY OT.DataOrca DESC 
+ERROR - 2017-06-03 16:31:39 --> Severity: Notice --> Undefined index: idApp_Procedimento C:\xampp\htdocs\sistema2\application\views\orcatrata\form_orcatrata.php 780
+ERROR - 2017-06-03 16:31:39 --> Severity: Notice --> Undefined index: idApp_ParcelasRec C:\xampp\htdocs\sistema2\application\views\orcatrata\form_orcatrata.php 781
+ERROR - 2017-06-03 16:31:40 --> Query error: Unknown column 'arquivos' in 'where clause' - Invalid query: SELECT * FROM App_OrcaTrata WHERE idApp_OrcaTrata = arquivos
+ERROR - 2017-06-03 16:32:16 --> Severity: Notice --> Undefined index: idApp_Procedimento C:\xampp\htdocs\sistema2\application\views\orcatrata\form_orcatrata.php 780
+ERROR - 2017-06-03 16:32:16 --> Severity: Notice --> Undefined index: idApp_ParcelasRec C:\xampp\htdocs\sistema2\application\views\orcatrata\form_orcatrata.php 781
+ERROR - 2017-06-03 16:32:39 --> Query error: Unknown column 'arquivos' in 'where clause' - Invalid query: SELECT * FROM App_Cliente WHERE idApp_Cliente = arquivos
+ERROR - 2017-06-03 16:36:01 --> Query error: Unknown column 'arquivos' in 'where clause' - Invalid query: SELECT * FROM App_Profissional WHERE idApp_Profissional = arquivos
+ERROR - 2017-06-03 16:36:57 --> Query error: Unknown column 'arquivos' in 'where clause' - Invalid query: SELECT * FROM App_Cliente WHERE idApp_Cliente = arquivos
+ERROR - 2017-06-03 16:38:00 --> Query error: Unknown column 'arquivos' in 'where clause' - Invalid query: SELECT * FROM App_Cliente WHERE idApp_Cliente = arquivos
+ERROR - 2017-06-03 16:38:47 --> Query error: Unknown column 'arquivos' in 'where clause' - Invalid query: SELECT * FROM App_Cliente WHERE idApp_Cliente = arquivos
+ERROR - 2017-06-03 16:39:44 --> Severity: Notice --> Undefined variable: data C:\xampp\htdocs\sistema2\application\controllers\Login.php 479
+ERROR - 2017-06-03 16:40:10 --> Query error: Unknown column 'arquivos' in 'where clause' - Invalid query: SELECT * FROM App_Consulta WHERE idApp_Consulta = arquivos
+ERROR - 2017-06-03 16:40:33 --> Query error: Unknown column 'arquivos' in 'where clause' - Invalid query: SELECT * FROM App_Consulta WHERE idApp_Consulta = arquivos
+ERROR - 2017-06-03 16:41:12 --> Query error: Unknown column 'arquivos' in 'where clause' - Invalid query: SELECT * FROM App_Consulta WHERE idApp_Consulta = arquivos
+ERROR - 2017-06-03 16:41:36 --> Query error: Unknown column 'arquivos' in 'where clause' - Invalid query: SELECT * FROM App_Consulta WHERE idApp_Consulta = arquivos
+ERROR - 2017-06-03 17:09:14 --> Severity: Notice --> Undefined index: Cliente C:\xampp\htdocs\sistema2\application\views\despesas\form_despesas.php 678
+ERROR - 2017-06-03 17:09:14 --> Severity: Notice --> Undefined variable: procedimento C:\xampp\htdocs\sistema2\application\views\despesas\form_despesas.php 681
+ERROR - 2017-06-03 17:09:14 --> Severity: Notice --> Undefined index: idApp_ParcelasRec C:\xampp\htdocs\sistema2\application\views\despesas\form_despesas.php 682
+ERROR - 2017-06-03 17:09:16 --> Query error: Unknown column 'arquivos' in 'where clause' - Invalid query: SELECT * FROM App_Despesas WHERE idApp_Despesas = arquivos
+ERROR - 2017-06-03 17:09:57 --> Severity: Notice --> Undefined index: Cliente C:\xampp\htdocs\sistema2\application\views\despesas\form_despesas.php 678
+ERROR - 2017-06-03 17:09:57 --> Severity: Notice --> Undefined variable: procedimento C:\xampp\htdocs\sistema2\application\views\despesas\form_despesas.php 681
+ERROR - 2017-06-03 17:09:57 --> Severity: Notice --> Undefined index: idApp_ParcelasRec C:\xampp\htdocs\sistema2\application\views\despesas\form_despesas.php 682
+ERROR - 2017-06-03 17:12:38 --> Severity: Notice --> Undefined index: Cliente C:\xampp\htdocs\sistema2\application\views\despesas\form_despesas.php 678
+ERROR - 2017-06-03 17:12:38 --> Severity: Notice --> Undefined variable: procedimento C:\xampp\htdocs\sistema2\application\views\despesas\form_despesas.php 681
+ERROR - 2017-06-03 17:12:38 --> Severity: Notice --> Undefined index: idApp_ParcelasRec C:\xampp\htdocs\sistema2\application\views\despesas\form_despesas.php 682
+ERROR - 2017-06-03 17:13:59 --> Severity: Notice --> Undefined index: Cliente C:\xampp\htdocs\sistema2\application\views\despesas\form_despesas.php 678
+ERROR - 2017-06-03 17:13:59 --> Severity: Notice --> Undefined variable: procedimento C:\xampp\htdocs\sistema2\application\views\despesas\form_despesas.php 681
+ERROR - 2017-06-03 17:13:59 --> Severity: Notice --> Undefined variable: parcelaspag C:\xampp\htdocs\sistema2\application\views\despesas\form_despesas.php 682
+ERROR - 2017-06-03 17:14:51 --> Severity: Notice --> Undefined index: Cliente C:\xampp\htdocs\sistema2\application\views\despesas\form_despesas.php 678
+ERROR - 2017-06-03 17:14:51 --> Severity: Notice --> Undefined variable: procedimento C:\xampp\htdocs\sistema2\application\views\despesas\form_despesas.php 681
+ERROR - 2017-06-03 17:14:51 --> Severity: Notice --> Undefined index: idApp_ParcelasRec C:\xampp\htdocs\sistema2\application\views\despesas\form_despesas.php 682
+ERROR - 2017-06-03 17:15:14 --> Severity: Notice --> Undefined index: auditoriaitem C:\xampp\htdocs\sistema2\application\controllers\Despesas.php 709
+ERROR - 2017-06-03 17:15:37 --> Severity: Notice --> Undefined index: Cliente C:\xampp\htdocs\sistema2\application\views\despesas\form_despesas.php 678
+ERROR - 2017-06-03 17:15:37 --> Severity: Notice --> Undefined variable: procedimento C:\xampp\htdocs\sistema2\application\views\despesas\form_despesas.php 681
+ERROR - 2017-06-03 17:15:37 --> Severity: Notice --> Undefined index: idApp_ParcelasRec C:\xampp\htdocs\sistema2\application\views\despesas\form_despesas.php 682
--- application/views/relatorio/list_despesas.php
+++ application/views/relatorio/list_despesas.php
@@ -44,72 +44,73 @@
 
     </div>	
 	
-</div>
 
-<div class="container-fluid">
-    <div class="row">
 
-        <div>
+	<div class="container-fluid">
+		<div class="row">
 
-            <table class="table table-bordered table-condensed table-hover">
+			<div>
 
-                <thead>
-                    <tr>
-                        <th class="active">Id Despesa</th>
-						<th class="active">Despesa</th>
-						<th class="active">Tipo de Desp.</th>
-                        <th class="active">Data da Despesa</th>
-                        <!--<th class="active">Data Entrada</th>
-                        <th class="active">Valor Entrada</th>-->
-						<th class="active">Desp. Aprov.?</th>
-						<th class="active">Desp. Concl.?</th>
-						<th class="active">Desp. Quit.?</th>						
-                        <th class="active">Parcela</th>
-                        <th class="active">Data do Venc.</th>
-                        <th class="active">Valor À Pagar</th>
-                        <th class="active">Data do Pagam.</th>
-                        <th class="active">Valor Pago</th>
-                        <th class="active">Parc. Quit.?</th>
-                    </tr>
-                </thead>
+				<table class="table table-bordered table-condensed table-hover">
 
-                <tbody>
+					<thead>
+						<tr>
+							<th class="active">Id Despesa</th>
+							<th class="active">Despesa</th>
+							<th class="active">Tipo de Desp.</th>
+							<th class="active">Data da Despesa</th>
+							<!--<th class="active">Data Entrada</th>
+							<th class="active">Valor Entrada</th>-->
+							<th class="active">Desp. Aprov.?</th>
+							<th class="active">Desp. Concl.?</th>
+							<th class="active">Desp. Quit.?</th>						
+							<th class="active">Parcela</th>
+							<th class="active">Data do Venc.</th>
+							<th class="active">Valor À Pagar</th>
+							<th class="active">Data do Pagam.</th>
+							<th class="active">Valor Pago</th>
+							<th class="active">Parc. Quit.?</th>
+						</tr>
+					</thead>
 
-                    <?php
-                    foreach ($report->result_array() as $row) {
+					<tbody>
 
-                        #echo '<tr>';
-                        echo '<tr class="clickable-row" data-href="' . base_url() . 'despesas/alterar/' . $row['idApp_Despesas'] . '">';
-                            echo '<td>' . $row['idApp_Despesas'] . '</td>';
-							echo '<td>' . $row['Despesa'] . '</td>';
-							echo '<td>' . $row['TipoDespesa'] . '</td>';
-                            echo '<td>' . $row['DataDespesas'] . '</td>';
-                           # echo '<td>' . $row['DataEntradaOrca'] . '</td>';
-                           # echo '<td class="text-right">R$ ' . $row['ValorEntradaOrca'] . '</td>';
-							echo '<td>' . $row['AprovadoDespesas'] . '</td>';
-							echo '<td>' . $row['ServicoConcluidoDespesas'] . '</td>';
-							echo '<td>' . $row['QuitadoDespesas'] . '</td>';							
-                            echo '<td>' . $row['ParcelaPagaveis'] . '</td>';
-                            echo '<td>' . $row['DataVencimentoPagaveis'] . '</td>';
-                            echo '<td class="text-left">R$ ' . $row['ValorParcelaPagaveis'] . '</td>';
-                            echo '<td>' . $row['DataPagoPagaveis'] . '</td>';
-                            echo '<td class="text-left">R$ ' . $row['ValorPagoPagaveis'] . '</td>';
-                            echo '<td>' . $row['QuitadoPagaveis'] . '</td>';
-                        echo '</tr>';
-                    }
-                    ?>
+						<?php
+						foreach ($report->result_array() as $row) {
 
-                </tbody>
+							#echo '<tr>';
+							echo '<tr class="clickable-row" data-href="' . base_url() . 'despesas/alterar/' . $row['idApp_Despesas'] . '">';
+								echo '<td>' . $row['idApp_Despesas'] . '</td>';
+								echo '<td>' . $row['Despesa'] . '</td>';
+								echo '<td>' . $row['TipoDespesa'] . '</td>';
+								echo '<td>' . $row['DataDespesas'] . '</td>';
+							   # echo '<td>' . $row['DataEntradaOrca'] . '</td>';
+							   # echo '<td class="text-right">R$ ' . $row['ValorEntradaOrca'] . '</td>';
+								echo '<td>' . $row['AprovadoDespesas'] . '</td>';
+								echo '<td>' . $row['ServicoConcluidoDespesas'] . '</td>';
+								echo '<td>' . $row['QuitadoDespesas'] . '</td>';							
+								echo '<td>' . $row['ParcelaPagaveis'] . '</td>';
+								echo '<td>' . $row['DataVencimentoPagaveis'] . '</td>';
+								echo '<td class="text-left">R$ ' . $row['ValorParcelaPagaveis'] . '</td>';
+								echo '<td>' . $row['DataPagoPagaveis'] . '</td>';
+								echo '<td class="text-left">R$ ' . $row['ValorPagoPagaveis'] . '</td>';
+								echo '<td>' . $row['QuitadoPagaveis'] . '</td>';
+							echo '</tr>';
+						}
+						?>
 
-                <tfoot>
-                    <tr>
-                        <th colspan="3" class="active">Total encontrado: <?php echo $report->num_rows(); ?> resultado(s)</th>
-                    </tr>
-                </tfoot>
-            </table>
+					</tbody>
 
-        </div>
+					<tfoot>
+						<tr>
+							<th colspan="3" class="active">Total encontrado: <?php echo $report->num_rows(); ?> resultado(s)</th>
+						</tr>
+					</tfoot>
+				</table>
 
-    </div>
+			</div>
 
+		</div>
+
+	</div>
 </div>
