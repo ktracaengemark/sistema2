baa4e5ec627c6d83b922601c763df1abc5424b12
ktracaengemark-Itau <marciorodeng@gmail.com>
2017-06-04 10:04:51 -0300

    Aniversariantes Profissionais e Contatos

---
+++ application/logs/log-2017-06-04.php
@@ -0,0 +1,9 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2017-06-04 14:59:40 --> Severity: Notice --> Undefined index: idApp_Profissional C:\xampp\htdocs\sistema2\application\views\contatocliente\list_contatocliente.php 53
+ERROR - 2017-06-04 14:59:40 --> Severity: Notice --> Undefined index: idApp_ContatoProf C:\xampp\htdocs\sistema2\application\views\contatocliente\list_contatocliente.php 53
+ERROR - 2017-06-04 14:59:40 --> Severity: Notice --> Undefined index: idApp_Profissional C:\xampp\htdocs\sistema2\application\views\contatocliente\list_contatocliente.php 56
+ERROR - 2017-06-04 14:59:41 --> 404 Page Not Found: Faviconico/index
+ERROR - 2017-06-04 15:00:26 --> Severity: Notice --> Undefined index: idApp_Profissional C:\xampp\htdocs\sistema2\application\views\contatocliente\list_contatocliente.php 53
+ERROR - 2017-06-04 15:00:26 --> Severity: Notice --> Undefined index: idApp_ContatoProf C:\xampp\htdocs\sistema2\application\views\contatocliente\list_contatocliente.php 53
+ERROR - 2017-06-04 15:00:26 --> Severity: Notice --> Undefined index: idApp_Profissional C:\xampp\htdocs\sistema2\application\views\contatocliente\list_contatocliente.php 56
